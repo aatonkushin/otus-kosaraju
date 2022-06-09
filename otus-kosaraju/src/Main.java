29ad887f704de9dc3d80d0d3e804242876dd393b
anton <aatonkushin@gmail.com>
2022-06-09 20:46:43 +0500

    Печать компонентов сильной связности

--- otus-kosaraju/src/Main.java
+++ otus-kosaraju/src/Main.java
@@ -29,7 +29,7 @@ public class Main {
         }
         System.out.println("\nКомпоненты сильной связности : ");
 
-        // Печать сильно
+        // Печать компонентов сильной связности
         List<List<Integer>> scComponents = k.getSCComponents(g);
         System.out.println(scComponents);
     }
